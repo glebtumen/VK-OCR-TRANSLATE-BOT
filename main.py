3bbbb2722d10d761f37169ecba71ce77a74ffb4d
Gleb Tumen <82389655+glebtumen@users.noreply.github.com>
2021-12-23 12:22:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,58 @@
+# requirements
+# googletrans==3.1.0a0
+# vk_api
+# requests
+# google-cloud-vision
+
 import vk_api
-from googletrans import Translator
-from vk_api.longpoll import VkLongPoll, VkEventType
 import requests
 import json
+import os
+from googletrans import Translator
+from vk_api.longpoll import VkLongPoll, VkEventType
+from google.cloud import vision
+from google.cloud.vision_v1 import AnnotateImageResponse
 
-vk_session = vk_api.VkApi(token='TAKE_THIS_TOKEN_FROM_YOUR_GROUP_VK')
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'get it from Google cloud'
+client = vision.ImageAnnotatorClient()
+
+
+vk_session = vk_api.VkApi(token='get it from VK group')
 longpoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
 
 translator = Translator()
 
+
 #idk like it is just a function
-def f(): 
+def f():
     pass
 
-#Main ocr thing that reads text from images. It is free so quality of reading is not impressive
-#To get api you need to register on their cite
-def ocr_space_url(url, overlay=False, api_key='PUT_YOUR_KEY_HERE', language='eng'):
-    """ OCR.space API request with remote file.
-        Python3.5 - not tested on 2.7
-    :param url: Image url.
-    :param overlay: Is OCR.space overlay required in your response.
-                    Defaults to False.
-    :param api_key: OCR.space API key.
-                    Defaults to 'helloworld'.
-    :param language: Language code to be used in OCR.
-                    List of available language codes can be found on https://ocr.space/OCRAPI
-                    Defaults to 'en'.
-    :return: Result in JSON format.
+def detect_text_uri(uri):
+    """Detects text in the file located in Google Cloud Storage or on the Web.
     """
+    image = vision.Image()
+    image.source.image_uri = uri
 
-    payload = {'apikey': api_key,
-               'url': url,
-               'language': language,
-               'scale': True,
-               'isTable': True,
-               }
-    r = requests.post('https://api.ocr.space/parse/image',
-                      data=payload,
-                      )
-    return r.content.decode()
+    response = client.text_detection(
+        image=image,
+        image_context={"language_hints": ["bn"]},)
 
-#Cycle for listening events such as messages and photos
+    serialized_proto_plus = AnnotateImageResponse.serialize(response)
+    response = AnnotateImageResponse.deserialize(serialized_proto_plus)
+    # print(response.full_text_annotation.text)
+
+    # serialize / deserialize json
+    response_json = AnnotateImageResponse.to_json(response)
+    response = json.loads(response_json)
+    l_response = response['fullTextAnnotation']['text']
+    l_response = l_response.replace('\n',' ')
+    return l_response
+
+
+
 for event in longpoll.listen():
-    
+
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:  #translates only text messages
         mesag = event.text.capitalize()
         result = translator.translate(mesag, src='en', dest='ru')
@@ -52,22 +61,26 @@ for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me: #translates text from photos
         message_id = event.message_id
         msg = vk.messages.getById(message_ids=message_id)
+        # print(msg)
         type = msg['items'][0]['attachments'] #get type of attachments
+        # print(type)
         if not type:
-            f()
+            vk.messages.send(peer_id=event.peer_id, random_id=0, message='Пожалуйста, отправьте изображение')
         else:
-            owner_id = event.peer_id
-            access_key = msg['items'][0]['attachments'][0]['photo']['access_key']
-            url = msg['items'][0]['attachments'][0]['photo']['sizes'][4]['url']
-            photo_id = msg['items'][0]['attachments'][0]['photo']['id']
-            photo = f'photo{owner_id}_{photo_id}_{access_key}' 
+            type_type = msg['items'][0]['attachments'][0]['type']
+            if type_type != 'photo':
+                vk.messages.send(peer_id=event.peer_id, random_id=0, message='Пожалуйста, отправьте изображение')
+            else:
+                owner_id = event.peer_id
+                access_key = msg['items'][0]['attachments'][0]['photo']['access_key']
+                url_photo = msg['items'][0]['attachments'][0]['photo']['sizes'][4]['url']
 
-            # vk.messages.send(peer_id=event.peer_id, random_id=0, attachment=photo) #пересылает пост обратно пользователю
+                super_response = detect_text_uri(url_photo)
 
-            test_url = ocr_space_url(url=url)
-            test_url = json.loads(test_url)
-            text_detected = test_url.get('ParsedResults')[0].get('ParsedText')
-            #print(text_detected)
-            result_translated = translator.translate(text_detected, src='en', dest='ru')
-            vk.messages.send(peer_id=event.peer_id, random_id=0, message=result_translated.text)
+                if super_response == '':
+                    vk.messages.send(peer_id=event.peer_id, random_id=0,
+                                     message='Невозможно перевести. Плохое качество изображения.')
+                else:
+                    result_translated = translator.translate(super_response, src='en', dest='ru')
+                    vk.messages.send(peer_id=event.peer_id, random_id=0, message=result_translated.text)
 
